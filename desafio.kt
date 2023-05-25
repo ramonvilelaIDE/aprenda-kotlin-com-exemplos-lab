a86b50607a114088c3ae82abcd20d5ba5364d23b
ramonvilelaIDE <118687542+ramonvilelaIDE@users.noreply.github.com>
2023-05-25 16:59:01 -0300

    first commit

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,36 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-class Usuario
+data class Usuario (val nome: String) {
+    usuario = nome
+}
+	
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, var duracao: Int = 60, val nivel: Nivel){
+    
+}
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, val conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    fun matricular(vararg usuario: Usuario ){
+        //TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    	
+       return println("$usuario $inscritos")
+        
+    
+        
     }
 }
 
+
+
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    /*TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
+    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")*/
+    val usuario: Usuario = "Aluno"
+    usuario.matricular(usuario)
+   
 }
